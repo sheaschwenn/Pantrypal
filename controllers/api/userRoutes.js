2ce4bb6455668b4124583d2aecdc1bdb5cafbc26
sheaschwenn <sheaschwenn@gmail.com>
2023-05-09 13:12:20 -0700

    userroutes update

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,19 +1,23 @@
 const router = require('express').Router();
 const {User} = require('../../models');
 
-router.post('/', async (req,res) => {
-    try{
-        const userData = await User.create(req.body)
+router.post('/', async (req, res) => {
+  try {
+      const newUser = await User.create({
+          username: req.body.username,
+          password: req.body.password,
+      });
 
-        req.session.save(() =>{
-            req.session.user_id = userData.id
-            req.session.username = userData.username
-            req.session.logged_in = true
-        })
-        res.status(200).json(userData)
-    }catch(err){
-        res.status(500).json(err)
-    }
+      req.session.save(() => {
+          req.session.user_id = newUser.id;
+          req.session.username = newUser.username;
+          req.session.logged_in = true;
+
+          res.json(newUser);
+      });
+  } catch (err) {
+      res.status(500).json(err);
+  }
 })
 
 router.post('/login', async (req,res)=>{
--- controllers/dashboardRoute.js
+++ controllers/dashboardRoute.js
@@ -2,3 +2,11 @@ const router = require('express').Router();
 const {User, Item, UserItem} = require('../models')
 const withAuth = require('../utils/auth')
 
+
+// router.get('/', async (req,res)=>{
+//     try{
+//         const itemData = await Item.findAll({
+
+//         })
+//     }
+// })
