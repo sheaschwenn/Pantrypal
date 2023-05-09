54405d723b8cc9fc0bb325ebae50856ce9601122
sheaschwenn <sheaschwenn@gmail.com>
2023-05-09 11:01:57 -0700

    index to join tables finished

--- models/index.js
+++ models/index.js
@@ -1,6 +1,26 @@
 const User = require("./User");
-const List = require("./Item");
+const Item = require("./Item");
+const UserItem = require("./UserItem")
 
-List.belongsTo(User, {
-  foreignKey: "user_id",
-});
+// List.belongsTo(User, {
+//   foreignKey: "user_id",
+// });
+
+// items belong to many users through useritem
+Item.belongsToMany(User,{
+    through: UserItem,
+    foreignKey: 'item_id'
+})
+
+// users belong to many items through useritems 
+
+User.belongsToMany(Item,{
+    through: UserItem,
+    foreignKey: 'user_id'
+})
+
+module.exports = {
+    User,
+    Item,
+    UserItem
+}
