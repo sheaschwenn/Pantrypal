001d1fed1139ef1b1e752fed4ed0a0c4bcbc1d2a
sheaschwenn <sheaschwenn@gmail.com>
2023-05-09 11:52:05 -0700

    prettier added

--- controllers/landingRoutes.js
+++ controllers/landingRoutes.js
@@ -1,19 +1,19 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', (req,res) =>{
-    if(req.session.logged_in){
-        res.redirect('/dashboard');
-        return
-    }
-    res.render('login')
-})
+router.get("/", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/dashboard");
+    return;
+  }
+  res.render("login");
+});
 
-router.get('/signup', (req,res) =>{
-    if(req.session.logged_in){
-        res.redirect('/dashboard');
-        return
-    }
-    res.render('signup')
-})
+router.get("/signup", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/dashboard");
+    return;
+  }
+  res.render("signup");
+});
 
-module.exports = router
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,26 +1,22 @@
 const User = require("./User");
 const Item = require("./Item");
-const UserItem = require("./UserItem")
-
-// List.belongsTo(User, {
-//   foreignKey: "user_id",
-// });
+const UserItem = require("./UserItem");
 
 // items belong to many users through useritem
 Item.belongsToMany(User, {
-    through: UserItem,
-    foreignKey: 'item_id'
-})
+  through: UserItem,
+  foreignKey: "item_id",
+});
 
-// users belong to many items through useritems 
+// users belong to many items through useritems
 
 User.belongsToMany(Item, {
-    through: UserItem,
-    foreignKey: 'user_id'
-})
+  through: UserItem,
+  foreignKey: "user_id",
+});
 
 module.exports = {
-    User,
-    Item,
-    UserItem
-}
+  User,
+  Item,
+  UserItem,
+};
