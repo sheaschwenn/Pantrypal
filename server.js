7b3adeb96b9b94708e51adc9eacad2e457528e25
seantamturk <128218653+seantamturk@users.noreply.github.com>
2023-05-10 15:48:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,29 +2,38 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-
-const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const dotenv = require('dotenv');
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
-
+// Sets up session and connect to our Sequelize db
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    // maxAge sets the maximum age for the session to be active. Listed in milliseconds.
+    maxAge: 24*60*60*1000, 
+    httpOnly: false, 
+    secure: true,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
+  // Sets up session store
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
-
 app.use(session(sess));
+
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
