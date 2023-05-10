809a96b3bca27bd023a5d2efb519c1e9f0eb6fab
sheaschwenn <sheaschwenn@gmail.com>
2023-05-10 09:26:46 -0700

    userItem route to create a UserItem row based on users search

--- controllers/api/itemRoutes.js
+++ controllers/api/itemRoutes.js
@@ -1,5 +1,21 @@
 const router = require('express').Router();
-
+const withAuth = require('../../utils/auth')
 const {User, Item, UserItem} = require('../../models')
 
 module.exports = router
+
+router.get('/', withAuth, async (req,res) => {
+    const searchItem = req.query.search
+    try{
+        const findItem = await Item.findAll({
+            where:{
+                name: {
+                    [Op.like]: `%${searchItem}%`
+                }
+            }
+        })
+        res.status(200).json({name:findItem})
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
