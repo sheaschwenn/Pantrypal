8ded483aff4fcd4207e12720d0960aeb1a6d8918
seantamturk <128218653+seantamturk@users.noreply.github.com>
2023-05-11 01:22:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const dotenv = require('dotenv');
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const dotenv = require("dotenv");
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-
-
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+// Load env variables
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -25,10 +21,10 @@ const sess = {
   secret: process.env.SESSION_SECRET,
   cookie: {
     // maxAge sets the maximum age for the session to be active. Listed in milliseconds.
-    maxAge: 24*60*60*1000, 
-    httpOnly: false, 
+    maxAge: 24 * 60 * 60 * 1000,
+    httpOnly: false,
     secure: true,
-    sameSite: 'strict',
+    sameSite: "strict",
   },
   resave: false,
   saveUninitialized: true,
@@ -39,17 +35,16 @@ const sess = {
 };
 app.use(session(sess));
 
-
 // Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
