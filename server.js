b015fddbb81429e80737c306c454e9ed37526c4b
kenneth o cruz <cruzkenneth504@gmail.com>
2023-05-11 13:03:53 -0700

    deleted comments

--- server.js
+++ server.js
@@ -20,9 +20,7 @@ const sess = {
   cookie: {
     // maxAge sets the maximum age for the session to be active. Listed in milliseconds.
     maxAge: 24 * 60 * 60 * 1000,
-    // httpOnly: false,
-    // secure: false,
-    // sameSite: "strict",
+  
   },
   resave: false,
   saveUninitialized: true,
