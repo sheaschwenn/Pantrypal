7eed7d91f138aaa9ce2b7ec14f3d02055c8819b1
sheaschwenn <sheaschwenn@gmail.com>
2023-05-10 17:46:52 -0700

    dynamically created days until expiration

--- utils/helpers.js
+++ utils/helpers.js
@@ -9,5 +9,12 @@ expiration_date: (date_bought, daysUntilExpiration) => {
     return dayjs(date_bought).add(daysUntilExpiration, 'day').format('MM/DD/YYYY')
 },
 
+days_until_expiration : (date_bought, daysUntilExpiration) => {
+    const expirationDate =  dayjs(date_bought).add(daysUntilExpiration, 'day').format('MM/DD/YYYY');
 
+    const currentDate = dayjs();
+    const daysLeft = dayjs(expirationDate).diff(currentDate, 'day');
+
+    return daysLeft
+},
 }
