1a2992366344a660612c71388f3d62a125dc6e21
seantamturk <128218653+seantamturk@users.noreply.github.com>
2023-05-11 01:38:26 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const dotenv = require("dotenv");
 
-
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const helpers = require("./utils/helpers");
@@ -15,6 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Sets up session and connect to our Sequelize db
+dotenv.config();
 const sess = {
   secret: process.env.SESSION_SECRET,
   cookie: {
@@ -33,16 +33,19 @@ const sess = {
 };
 app.use(session(sess));
 
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
 // Inform Express.js on which template engine to use
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () => console.log('Now listening'));
 });
